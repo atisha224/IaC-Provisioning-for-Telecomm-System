76f672c6fb8ed50d0d3d1a199d34bf2cf1702a33
Atisha Jain <atishajain224@gmail.com>
2026-04-21 22:21:42 +0530

    added remote state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-atisha"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
